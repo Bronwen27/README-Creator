06c182b24358580f07c1260da03f1ca5716aeaea
Bronwen27 <118174027+Bronwen27@users.noreply.github.com>
2023-02-26 14:30:23 +0000

    added questions and added loading symbol at the end

--- index.js
+++ index.js
@@ -25,19 +25,57 @@ const questions = [
 
  {
    input: 'input',
-   questionName: 'userStory',
-   message: 'Please enter your repository name'
+   questionName: 'description',
+   message: 'Please write a breif description of your prject along with programmes and languages used'
+},
+
+{
+   input: 'input',
+   questionName: 'usage',
+   message: 'Please write a breif summary of how to use your project, you may wish to include some image links'
+},
+
+{
+   input: 'input',
+   questionName: 'commands',
+   message: 'please enter any commands that will be needed to install/ run  or test any parts of your project'
+},
+
+{
+   input: 'input',
+   questionName: 'deployedLink',
+   message: 'Please enter your deployment link'
+},
+
+{
+   input: 'input',
+   questionName: 'license',
+   message: 'Please enter the license that you are under'
 },
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+
+  fs.writeFileSync(fileName, data);
+  console.log('File saved!');
 }
 
-// function to initialize program
-function init() {
+writeToFile('README.md', '# My Project\nThis is a description of my project.');
 
+
+// function to initialize program
+const inquirer = require('inquirer');
+const ora = require('ora');
+
+function init() {
+  const spinner = ora('Loading questions...').start();
+
+  inquirer.createPromptModule(questions).then((response) => {
+    spinner.stop();
+    console.log(response);
+  });
 }
 
 // function call to initialize program
