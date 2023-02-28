07be428721503c512703988aad70d9a19b2ddf0c
Bronwen27 <118174027+Bronwen27@users.noreply.github.com>
2023-02-28 00:51:01 +0000

    changed input to list so that choices appear in terminal

--- index.js
+++ index.js
@@ -8,56 +8,56 @@ const questions = [
  {
     type: 'input',
     name: 'title',
-    message: 'Please enter your repository title'
+    message: 'Please enter your repository title: '
  },
  
  {
    type: 'input',
    name: 'description',
-   message: 'Please write a brief description of your project along with programmes and languages used'
+   message: 'Please write a brief description of your project along with programmes and languages used: '
 },
 
 {
    type: 'input',
    name: 'installation',
-   message: 'please describe any commands that will be needed to install/run or test any parts of your project'
+   message: 'please describe any commands that will be needed to install/run or test any parts of your project: '
 },
 
 {
    type: 'input',
    name: 'usage',
-   message: 'Please write a brief summary of how to use your project, you may wish to include some image links'
+   message: 'Please write a brief summary of how to use your project, you may wish to include some image links: '
 },
 
 {
-   type: 'input',
+   type: 'list',
    name: 'license',
-   message: 'Please enter the license name that you are under',
+   message: 'Please enter the license name that you are under: ',
    choices: ['MIT', 'Apache 2.0', 'GPLv3', 'BSD 3-Clause', 'Unlicensed']
 },
 
 {
    type: 'input',
    name: 'contribute',
-   message: 'please write whether the user can contribute to this project and include the channels they should use'
+   message: 'please write whether the user can contribute to this project and include the channels they should use: '
 },
 
 {
    type: 'input',
    name: 'tests',
-   message: 'Please enter any commands that are needed to run tests'
+   message: 'Please enter any commands that are needed to run tests: '
 },
 
 {
    type: 'input',
    name: 'githubName',
-   message: 'Please enter your github user-name'
+   message: 'Please enter your github user-name: '
 },
 
 {
    type: 'input',
    name: 'email',
-   message: 'Please enter your email address'
+   message: 'Please enter your email address: '
 },
 
 ];
