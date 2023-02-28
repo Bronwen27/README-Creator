dc7db2c838e8330b677622145c8ff758c18d709f
Bronwen27 <118174027+Bronwen27@users.noreply.github.com>
2023-02-28 01:11:06 +0000

    added license badges to markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,18 @@
+// function to add license badges
+function renderLicenseBadge(license) {
+  const licenses = {
+    "MIT": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    "Apache 2.0": "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+    "GPLv3": "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+    "BSD 3-Clause": "[![License: BSD 3-Clause](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
+    "Unlicensed": "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+  };
+  return licenses[license];
+}
+
+// export the renderLicenseBadge function for use in other modules
+module.exports = renderLicenseBadge;
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
